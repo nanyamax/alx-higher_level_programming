95324014071de1b8ab7f2c4b6e4f057996bf43bd
nanyamax <ifunanyasunday7@gmail.com>
2022-12-01 09:06:47 -0800

    dicovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 
+if __name__ == '__main__':
+    def_names = dir(hidden_4)
 
-def main():
-    l = dir(hidden_4)
-    for i in range(len(l)):
-        if(l[i][0] != '_'):
-            print("{}".format(l[i]))
-
-
-if __name__ == "__main__":
-    main()
+    for i in range(len(def_names)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
