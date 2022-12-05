5130468413368d2620364a18cc062422e39e6dbc
nanyamax <ifunanyasunday7@gmail.com>
2022-12-05 12:15:04 -0800

    task

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LISTS_H_
+#define LISTS_H_
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -7,12 +8,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +22,3 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
