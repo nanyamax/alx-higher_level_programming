5f7d3d9a292e9ae862947e34b2be144bc52163ab
nanyamax <ifunanyasunday7@gmail.com>
2022-12-05 05:16:03 -0800

    task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,29 @@
 #include "lists.h"
 /**
- * is_palindrome - function that check for palindrom
- * @head:list
- *Return: 0 or 1
+ * comparator - compare start and end position
+ * @head: the linked list
+ * @last: the end of the list
+ * Return: 1 true, 0 false
+ */
+int comparator(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (comparator(head, last->next) && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: the linked list
+ * Return: 1 true, 0 false
  */
 int is_palindrome(listint_t **head)
 {
-	const listint_t *current;
-	int len;
-	int i, j;
-	int arr[10000];
-
-	if (*head == NULL)
+	if (!head || !(*head))
 		return (1);
-	current = *head;
-	len = 0;
-	while (current != NULL)
-	{
-		current = current->next;
-		len++;
-	}
-	if (len == 1)
-		return (1);
-
-	current = *head;
-	i = 0;
-	while (current != NULL)
-	{
-		arr[i] = current->n;
-		i++;
-		current = current->next;
-	}
-	j = 0;
-	i--;
-	len--;
-	while (i >= (len / 2))
-	{
-		if (arr[i] != arr[j])
-			return (0);
-		i--;
-		j++;
-	}
-	return (1);
+	return (comparator(head, *head));
+}
