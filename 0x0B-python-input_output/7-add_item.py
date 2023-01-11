79ff34cedb553fa6050809778591ca25a3ee0a32
nanyamax <ifunanyasunday7@gmail.com>
2023-01-11 04:45:05 -0800

    fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
 try:
-    l = load_from_json_file("add_item.json")
+    leg = load_from_json_file("add_item.json")
 except ValueError:
-    l = []
+    leg = []
 save_to_json_file(l + sys.argv[1:], "add_item.json")
