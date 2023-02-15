d745e0e5e697d43f813eda36c0b7b2b1ced31cc1
nanyamax <ifunanyasunday7@gmail.com>
2023-02-14 17:27:47 -0800

    advanced sql task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,4 @@
--- converts database, table and field from utf8mb4 to utf8mb4_unicode
-
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8 COLLATE = utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.`first_table` CONVERT TO CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE hbtn_0c_0.`first_table` CHANGE name name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+-- Converts the entire database hbtn_0c_0 to UTF8.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
