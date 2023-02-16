a5bc47889885600e4001a617f3140b18db22d237
nanyamax <ifunanyasunday7@gmail.com>
2023-02-16 14:16:54 -0800

    sequel

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- creates a database , table , FK & PK.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
-	`id` INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	PRIMARY KEY(`id`),
+	`id` INT AUTO_INCREMENT  NOT NULL,
 	`state_id` INT NOT NULL,
 	FOREIGN KEY(`state_id`)
 	REFEERENCES `hbtn_0d_usa`.`states`(`id`)
