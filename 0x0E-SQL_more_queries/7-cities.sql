f983198c57a192287a100f30bb1815e873ea9de7
nanyamax <ifunanyasunday7@gmail.com>
2023-02-16 14:19:54 -0800

    sequel

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
 	`id` INT AUTO_INCREMENT  NOT NULL,
 	`state_id` INT NOT NULL,
 	FOREIGN KEY(`state_id`)
-	REFEERENCES `hbtn_0d_usa`.`states`(`id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`),
 	`name` VARCHAR(256) NOT NULL
 );
