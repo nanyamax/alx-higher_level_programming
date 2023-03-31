21cffbf2103c63c72245b8c08d455f0e3517e89b
nanyamax <ifunanyasunday7@gmail.com>
2023-03-31 06:11:07 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" Finds a peak in a list of unsorted integers using binary search algorithm."""
-def find_peak(list_of_integers):
+"""finds a peak in an algorithm."""
 
-    if len(list_of_integers) == 0:
+
+def find_peak(list_of_integers):
+    if list_of_integers == []:
         return None
-    
-    if len(list_of_integers) == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    
-    mid = len(list_of_integers) // 2
-    
-    if list_of_integers[mid] < list_of_integers[mid-1]:
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
-    elif list_of_integers[mid] < list_of_integers[mid+1]:
-        return find_peak(list_of_integers[mid+1:])
     else:
-        return list_of_integers[mid]
+        return find_peak(list_of_integers[mid + 1:])
 
