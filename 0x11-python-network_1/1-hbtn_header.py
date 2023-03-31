f2c0412d0de43cce2a0a59bae3fce8d36653f091
nanyamax <ifunanyasunday7@gmail.com>
2023-03-31 13:38:07 -0700

    network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,9 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     with urllib.request.urlopen(url) as response:
-    
         headers = response.headers
         x_request_id = headers.get('X-Request-Id')
         print(x_request_id)
