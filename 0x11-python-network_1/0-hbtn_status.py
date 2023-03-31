d3cfd4d1e00d2938335ef2497e696d312f1af242
nanyamax <ifunanyasunday7@gmail.com>
2023-03-31 13:34:12 -0700

    network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         html = response.read()
