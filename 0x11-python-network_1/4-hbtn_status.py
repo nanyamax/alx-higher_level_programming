53fd3e86008c7ec441e31e3098a28db01cc5c45c
nanyamax <ifunanyasunday7@gmail.com>
2023-03-31 14:10:47 -0700

    network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-#fetches a url status using request
+"""fetches a url status using request"""
 import requests
-url = https://alx-intranet.hbtn.io/status
-r = requests.get(url)
-print("Body response:")
+
+
+if __name__ == "__main__":
+    url = https://alx-intranet.hbtn.io/status
+    r = requests.get(url)
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
